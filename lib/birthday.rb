2ea685bc5c281519d0b07ec7372de934a6624adf
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 15:43:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,8 +13,8 @@ end
 
 
 def age_appropriate_birthday(birthday_kids)
-  if age < 13
-    birthday_kids.each do |kids_name, age|
+  birthday_kids.each do |kids_name, age|
+    if age < 13
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
